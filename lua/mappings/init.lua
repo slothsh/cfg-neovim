ecf4550f6875f37eb7069f356200e22778d53f43
Stefan Olivier <snowflake1194@gmail.com>
2022-02-20 23:51:13 +0200

    mapped <S-Up> and <S-Down> as navigation keys in insert and normal modes added mappings for Telescope git functions

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -5,19 +5,23 @@ local opts = {noremap = true, silent = true}
 
 vim.g.mapleader = ' '
 
-local xsafasf = 138929854
-local y       = 9592385
-
 -- Normal mode mappings
 map('n', '<C-b>', ':NvimTreeToggle<CR>:NvimTreeRefresh<CR>', opts)
-map('n', '<C-Up><Up>', ':FocusMaximise<CR>', opts)
-map('n', '<C-Down><Down>', ':FocusEqualise<CR>', opts)
+map('n', '<leader><Up>', ':FocusMaximise<CR>', opts)
+map('n', '<leader><Down>', ':FocusEqualise<CR>', opts)
 map('n', '<C-p>', ':Telescope find_files<CR>', opts)
 map('n', '<C-f>', ':Telescope live_grep<CR>', opts)
+map('n', '<leader><C-f>', ':Telescope grep_string<CR>', opts)
 map('n', '<M-Down>', ':m +1<CR>', opts)
 map('n', '<M-Up>', ':m -2<CR>', opts)
-map('n', '<S-Up>', ':lua duplicate_line(-1)<CR>', opts)
-map('n', '<S-Down>', ':lua duplicate_line(1)<CR>', opts)
+map('n', '<C-Up>', ':lua duplicate_line(-1)<CR>', opts)
+map('n', '<C-Down>', ':lua duplicate_line(1)<CR>', opts)
+map('n', '<S-Up>', '{', opts)
+map('n', '<S-Down>', '}', opts)
+map('n', '<leader><C-g>b', ':Telescope git_branches<CR>', opts)
+map('n', '<leader><C-g>f', ':Telescope git_files<CR>', opts)
+map('n', '<leader><C-g>s', ':Telescope git_status<CR>', opts)
+map('n', '<leader><C-g>c', ':Telescope git_commits<CR>', opts)
 
 -- Visual mode mappings
 map('v', '<leader>a', ':\'<,\'>Align =<CR>', opts)
@@ -28,5 +32,7 @@ map('i', '<M-BS>', '<C-w>', opts)
 map('i', '<M-Del>', '<C-o>de', opts)
 map('i', '<M-Down>', '<C-o>:m +1<CR>', opts)
 map('i', '<M-Up>', '<C-o>:m -2<CR>', opts)
-map('i', '<S-Up>', '<C-o>:lua duplicate_line(-1)<CR>', opts)
-map('i', '<S-Down>', '<C-o>:lua duplicate_line(1)<CR>', opts)
+map('i', '<C-Up>', '<C-o>:lua duplicate_line(-1)<CR>', opts)
+map('i', '<C-Down>', '<C-o>:lua duplicate_line(1)<CR>', opts)
+map('i', '<S-Up>', '<C-o>{', opts)
+map('i', '<S-Down>', '<C-o>}', opts)
