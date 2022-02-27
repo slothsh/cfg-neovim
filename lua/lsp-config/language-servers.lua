07c95aaf86cbd416adcd2ee043a31283435acc59
Stefan Olivier <snowflake1194@gmail.com>
2022-02-27 18:36:55 +0200

    added lsp config for tailwindcss

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -31,7 +31,7 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'clangd', 'tsserver', 'cmake', 'vuels', 'html', 'cssls', 'emmet_ls' }
+local servers = { 'pyright', 'clangd', 'tsserver', 'cmake', 'vuels', 'html', 'cssls', 'emmet_ls', 'tailwindcss' }
 for _, lsp in pairs(servers) do
     require('lspconfig')[lsp].setup {
         on_attach = on_attach,
