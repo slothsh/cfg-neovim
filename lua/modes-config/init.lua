5f8ec260783c5a3463f3c52aa7b49f79a84d3c4e
Stefan Olivier <snowflake1194@gmail.com>
2022-05-15 22:46:36 +0200

    changed colours for line mode highlighting

--- lua/modes-config/init.lua
+++ lua/modes-config/init.lua
@@ -1,1 +1,25 @@
-require('modes').setup()
+require('modes').setup({
+	colors = {
+		copy = "#f5c359",
+        delete = "#851f00",
+		insert = "#78ccc5",
+		visual = "#9745be",
+    },
+
+	-- Set opacity for cursorline and number background
+	line_opacity = 0.15,
+
+	-- Enable cursor highlights
+	set_cursor = true,
+
+	-- Enable cursorline initially, and disable cursorline for inactive windows
+	-- or ignored filetypes
+	set_cursorline = true,
+
+	-- Enable line number highlights to match cursorline
+	set_number = false,
+
+	-- Disable modes highlights in specified filetypes
+	-- Please PR commonly ignored filetypes
+	ignore_filetypes = { 'NvimTree', 'TelescopePrompt' }
+})
